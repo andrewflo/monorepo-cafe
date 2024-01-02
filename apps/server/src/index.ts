a208b6b55c939047b2051ca4091acf3ee1797cc4
Andrew Flores <andrewjflores1@gmail.com>
2024-01-02 04:16:45 +0100

    Add SHOP_URL to CORS allowed origins

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import productsRouter from './routes/products';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+const shopUrl = process.env.SHOP_URL || '';
 
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: [shopUrl] }));
 
 app.get('/', (req, res) => {
   return res.json({ hello: 'world' });
